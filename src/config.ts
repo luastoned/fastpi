43e75a55b08ac34d18d0547c2960b6e4c34c13db
LuaStoned <gluastoned@gmail.com>
2025-03-04 11:20:00 +0100

    feat: 📦️ updates

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const APP_HOST = process.env.APP_HOST || '0.0.0.0'; // 0.0.0.0 localhost
-export const APP_PORT = process.env.APP_PORT ? Number(process.env.APP_PORT) : 42000;
+export const APP_PORT = process.env.APP_PORT ? Number(process.env.APP_PORT) : 50010;
